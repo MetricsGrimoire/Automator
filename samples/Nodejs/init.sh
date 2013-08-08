57f36b3f3fd37d3172f5bb0e984cbe90b9366db3
Alvaro del Castillo <acs@bitergia.com>
2013-08-08 19:23:02 +0200

    Remove not needed dbs

--- samples/Nodejs/init.sh
+++ samples/Nodejs/init.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-
-if [ -d "../production/browser/data/db/" ]
+if [ -d "production/browser/data/db/" ]
 then
     echo "Project already configured"
+    cd ../..
+    echo ./launch.py -d `pwd`/samples/Nodejs
+    exit
 fi
+
+# Basic dirs
+mkdir -p json log scm tools production
 
 # SCM repos to analyze
 cd scm
@@ -20,6 +25,7 @@ cd ..
 git clone https://github.com/VizGrimoire/VizGrimoireJS.git
 cp -a VizGrimoireJS/* ../production/
 rm -rf ../production/browser/data/json/*
+rm -rf ../production/browser/data/db
 cp VizGrimoireJS/browser/data/json/viz_cfg.json ../production/browser/data/json/
 cp VizGrimoireJS/browser/data/json/project-info.json ../production/browser/data/json/
 mkdir -p ../production/browser/data/db/

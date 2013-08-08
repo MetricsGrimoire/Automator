f9447d0d9fc7f3dc6a451500372dbaecee38b9be
Alvaro del Castillo <acs@bitergia.com>
2013-08-08 19:13:49 +0200

    JSON updated by the Owl Bot

---
+++ samples/MongoDB/init.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+if [ -d "production/browser/data/db/" ]
+then
+    echo "Project already configured"
+    cd ../..
+    echo ./launch.py -d `pwd`/samples/MongoDB
+    exit
+fi
+
+# Basic dirs
+mkdir -p json log scm tools production
+
+# SCM repos to analyze
+cd scm
+git clone https://github.com/mongodb/mongo.git
+cd ..
+
+# Tools to do the analysis
+cd tools
+git clone https://github.com/VizGrimoire/VizGrimoireR.git
+mkdir r-lib
+cd VizGrimoireR
+R CMD INSTALL -l ../r-lib vizgrimoire
+cd ..
+git clone https://github.com/VizGrimoire/VizGrimoireJS.git
+cp -a VizGrimoireJS/* ../production/
+rm -rf ../production/browser/data/json/*
+rm -rf ../production/browser/data/db
+cp VizGrimoireJS/browser/data/json/viz_cfg.json ../production/browser/data/json/
+cp VizGrimoireJS/browser/data/json/project-info.json ../production/browser/data/json/
+mkdir -p ../production/browser/data/db/
+cd ..
+
+# Main directory
+cd ../..
+echo ./launch.py -d `pwd`/samples/MongoDB

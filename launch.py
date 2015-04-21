f7f08d98a0b889cb56d9b00d4d2cb0811444e3fb
Alvaro del Castillo <acs@bitergia.com>
2015-04-21 15:53:08 +0200

    [launch sortinghat] Added comment about sortinghat mode of loading and matching incrementally.

--- launch.py
+++ launch.py
@@ -899,7 +899,7 @@ def launch_sortinghat():
                       %(db_user, db_pass, db_ds, project_name.lower(), ds.get_name(), io_file_name, log_file)
         compose_msg(cmd, log_file)
         os.system(cmd)
-        # Load identities in sortinghat
+        # Load identities in sortinghat in incremental mode
         cmd = tools['sortinghat'] + " -u \"%s\" -p \"%s\" -d \"%s\" load --matching email-name -n %s >> %s 2>&1" \
                       %(db_user, db_pass, db_name, io_file_name, log_file)
         compose_msg(cmd, log_file)
